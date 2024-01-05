4a42139a6301647dcb10b344953e78c83eb033cb
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2024-01-05 17:02:40 +0900

    Time: 90 ms (87.25%), Space: 21.5 MB (48.89%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -3,9 +3,11 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         
-        frequency_dict = Counter(nums)
-        top_k_items = [item for item, _ in frequency_dict.most_common(k)]
-        return top_k_items
+        return list(zip(*collections.Counter(nums).most_common(k)))[0]
+        
+        # frequency_dict = Counter(nums)
+        # top_k_items = [item for item, _ in frequency_dict.most_common(k)]
+        # return top_k_items
         
         # frequency_dict = Counter(nums)
         # sorted_items = sorted(frequency_dict, key=frequency_dict.get, reverse=True)
