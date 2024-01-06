16ddc50e6ef179cab337c04cbfba71341c6dd564
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2024-01-06 17:27:18 +0900

    Time: 97 ms (87.65%), Space: 17.7 MB (17.77%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.py
@@ -3,11 +3,11 @@ class Solution:
         
         row_strengths = {}
         count = 0
-        
+
         for i in range(len(mat)):
             count = mat[i].count(1)
             row_strengths[i] = count
-        
+
         # 딕셔너리를 강도(count)를 기준으로 정렬
         sorted_weaker = sorted(row_strengths.items(), key=lambda x: x[1])
 
@@ -15,4 +15,21 @@ class Solution:
         # 튜플 형식일 때 for문에서는 key, value로 받아준다.
         for key, _ in sorted_weaker[:k]:
             result.append(key)
-        return result
+        return result    
+        
+        
+        
+#         def count_ones(row):
+#             count = 0
+#             for val in row:
+#                 if val == 1:
+#                     count += 1
+#                 else:
+#                     break
+#             return count
+
+#         row_strengths = [(count_ones(row), idx) for idx, row in enumerate(mat)]
+#         row_strengths.sort()
+
+#         result = [idx for strength, idx in row_strengths[:k]]
+#         return result
