0ddc10a25a44f54afab6e97295d6193736e5d65a
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2024-01-10 00:37:41 +0900

    Time: 43 ms (24.66%), Space: 17.3 MB (24.26%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -6,16 +6,16 @@ class MyStack:
 
     def push(self, x: int) -> None:
         self.queue.append(x)
-        
+    
+    # def change_index(self): 
+    # 함수로 top()이나 pop()할 때 지속적인 변경을 주고 싶지만, 결국에는 pop() top() 순서를 정하지 않으면 구현할 수 없는 것 같다. 결국 지금은 일회성으로 stack 느낌을 주는게 최선인 것 같다.
         for _ in range(len(self.queue) - 1):
             self.queue.append(self.queue.popleft())
-        
         
 
     def pop(self) -> int:
         return self.queue.popleft()
         
-
     def top(self) -> int:
         return self.queue[0]
         
