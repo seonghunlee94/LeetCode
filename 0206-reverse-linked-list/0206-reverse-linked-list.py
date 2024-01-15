5939c2091cf5196bf2b0fca97a9870789876f8ab
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2024-01-15 19:01:35 +0900

    Time: 37 ms (80.23%), Space: 18.3 MB (55.71%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,18 +5,15 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        # 이전 노드를 저장할 변수
-        prev_node = None
-        # 현재 노드를 가르킬 변수
-        current_node = head
+        node = head
+        prev = None
         
-        while current_node:
-            next_node = current_node.next # 현재 노드 연결 끊기 전 다음 노드 지정
-            current_node.next = prev_node # 현재 노드 연결 끊기(새로운 연결).
-            prev_node = current_node      # 새로운 노드에 현재 노드 저장
-            current_node = next_node      # 다음 노드를 현재 노드로 변경
+        while node:
+            next_node = node.next
+            node.next = prev
+            prev = node
+            node = next_node
             
-        return prev_node
-            
+        return prev
             
             
