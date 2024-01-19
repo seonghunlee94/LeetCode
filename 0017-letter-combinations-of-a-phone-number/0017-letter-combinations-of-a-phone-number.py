105845d2a8f5e6afd25bd53f36dd6cc290a8c06d
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2024-01-20 01:47:49 +0900

    Time: 38 ms (59.30%), Space: 16.5 MB (51.44%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,8 +1,9 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
         
-        if not digits:  # 입력이 빈 문자열인 경우
-            return []   # 빈 리스트 반환
+        # 입력이 빈 문자열인 경우
+        if not digits:  
+            return []   
 
         # 각 숫자에 매핑된 문자들
         keypad = {
@@ -16,20 +17,24 @@ class Solution:
             '9': 'wxyz'
         }
 
-        # 재귀적으로 문자 조합을 찾는 함수
-        def backtrack(index, path):
-            if index == len(digits):  # 현재 탐색 인덱스가 입력의 길이와 같으면
-                combinations.append(path)  # 조합 리스트에 결과 추가
+        # 숫자 조합으로 만들 수 있는 문자의 조합(재귀 함수).
+        def num_backtrack_combined_char(index, path):
+            
+            if index == len(digits):
+                combined_char.append(path)
                 return
-
-            current_digit = digits[index]  # 현재 숫자
-            for char in keypad[current_digit]:  # 현재 숫자에 해당하는 문자들에 대해 반복
-                backtrack(index + 1, path + char)  # 다음 숫자 조합을 찾기 위해 재귀 호출
-
-        combinations = []  # 모든 조합을 담을 리스트
-        backtrack(0, "")   # 재귀 함수 호출 시작
-        return combinations  # 모든 조합 반환
+            
+            current_digit = digits[index]
+            for char in keypad[current_digit]:
+                num_backtrack_combined_char(index + 1, path + char)
                 
+            
+
+        combined_char = []
+        num_backtrack_combined_char(0, "")
+        return combined_char
+        
+        
 
                 
                 
