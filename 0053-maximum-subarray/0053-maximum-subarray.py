9042fff7324e0304d0b76c6e50e8a4d37117f377
seonghunlee94 <85480460+seonghunlee94@users.noreply.github.com>
2024-01-22 13:45:50 +0900

    Time: 554 ms (71.67%), Space: 30.9 MB (59.60%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,7 +4,7 @@ class Solution:
         best_sum = -sys.maxsize
         current_sum = 0
         
-        # 최대 합계를 구하는 함수
+        # 최대 합계를 구하는 for문
         for num in nums:
             current_sum = max(num, current_sum + num)
             best_sum = max(best_sum, current_sum)
